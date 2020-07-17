40300e7f8478bb80276363cad8e84f053c7aed2a
Kiriti Komaragiri <Kiriti_Komaragiri@epam.com>
2020-07-18 02:33:35 +0530

    logs

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 // https://github.com/auth0/node-jsonwebtoken
-// refresh token: https://github.com/jppellerin/node-jsonwebtoken/tree/refresh-token
+// refresh token: https://github.com/jppellerin/node-jsonwebtoken#jwtrefreshtoken-expiresin-secretorprivatekey--callback
 // expiry: https://github.com/vercel/ms
 const cs = require('../constants');
 const HttpStatus = require('http-status-codes');
@@ -18,7 +18,7 @@ const auth = {
         if (!token) throw new APIError(cs.NO_TOKEN, HttpStatus.UNAUTHORIZED);
         try {
             const decoded = jwt.verify(token, secret);
-            req.user = decoded.user;
+            req.user = decoded ? decoded.data.user : '';
             next();
         } catch (err) {
             console.error(err);
@@ -26,13 +26,17 @@ const auth = {
         }
     },
 
-    refresh(req, res) {
+    refresh(token) {
         var originalDecoded = jwt.decode(token, { complete: true });
-        var refreshed = jwt.refresh(originalDecoded, 3600, secret);
+        var refreshed = jwt.refresh(originalDecoded, '7d', secret);
 
-        console.log(JSON.stringify(originalDecoded));
+        console.log('');
+        console.log('original ', JSON.stringify(originalDecoded));
+        console.log('');
         // new 'exp' value is later in the future.
-        console.log(JSON.stringify(jwt.decode(refreshed, { complete: true })));
+        console.log('refreshed ', JSON.stringify(jwt.decode(refreshed, { complete: true })));
+        console.log('');
+        // return jwt.decode(refreshed, { complete: true });
     }
 };
 
